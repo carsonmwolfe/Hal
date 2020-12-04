a6c9db3910f2b03ddeb068d13862cd19e699ca0c
Carson <46657662+Darkarea51@users.noreply.github.com>
2020-12-03 16:17:52 -0800

    Add files via upload

---
+++ Python/EssentialPackages.py
@@ -0,0 +1,19 @@
+import discord
+import Token
+import youtube_dl
+import os
+import re
+import urllib
+import copy
+import datetime
+import logging
+from typing import Dict
+from lxml import etree
+import json
+import ffmpeg
+import asyncio
+import requests
+import logging
+EMBEDCOLOR = 3447033
+        
+
