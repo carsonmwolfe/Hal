691f0832bd248ce8ad2a6e2c74b079e9e8494690
Carson <46657662+Darkarea51@users.noreply.github.com>
2020-12-03 16:08:58 -0800

    Add files via upload

---
+++ Hal2.0/Command.py
@@ -0,0 +1,15 @@
+import sys
+import os
+from EssentialPackages import *
+
+CREATOR_ID = 653386075095695361
+HAL_ID = 779155599622537226
+
+command = {}
+commands = []
+serverinfo = {}
+playerinfo = {}
+
+
+
+   
